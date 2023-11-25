f84314f0dd23d32ef79af583cf3bb1fc66948098
unknown <meherabm803@gmail.com>
2023-11-25 19:42:54 +0600

    fourth commit

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -8,6 +8,7 @@ router.get('/',userController.getAllUser)
 router.get('/:userId',userController.getSingleUser)
 router.put('/:userId',userController.updateUser)
 router.delete('/:userId',userController.deleteUser)
+// Orders routes
 router.put('/:userId/orders',userController.addNewProduct)
 router.get('/:userId/orders',userController.singleUserAllOrders)
 router.get('/:userId/total-price',userController.totalPriceOfOrders)
